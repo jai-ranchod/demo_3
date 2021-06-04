456182ac41043e83e7d6a7f151034f82527d2147
jai-ranchod <75948545+jai-ranchod@users.noreply.github.com>
2021-06-04 14:23:13 -0400

    Updating commentary

--- Data Visualizations - Titanic Data.R
+++ Data Visualizations - Titanic Data.R
@@ -1,12 +1,16 @@
+#####Loading Libraries and Introducing Data#####
 
+#We will be using the built in "Titanic" data set to show some visualizations that can help us better understand
+#the data.  The "Titanic" data set shows who survived the infamous crash as well as demographic and commercial
+#data about the passengers.
 
-options(digits = 3)    # report 3 significant digits
+if (!require("tidyverse")) install.packages("tidyverse")
+if (!require("titanic")) install.packages("titanic")
 library(tidyverse)
 library(titanic)
-#We can use the built in "titanic_train" data set to illustrate the use of various data visualizations.  These visualizations will show us information
-#about passengers about the Titanic, as well as survival rates from the wreck broken down by various factors.
 
-#####Initial Data Processing#####
+options(digits = 3)    # report 3 significant digits
+
 
 titanic <- titanic_train %>%
   select(Survived, Pclass, Sex, Age, SibSp, Parch, Fare) %>%
@@ -38,7 +42,7 @@ titanic %>%
   ggtitle("Distribution of Sex on the Titanic")
 
 #Here we see that younger passengers are more likely to be female while older passengers are more likely to be male with the transition happening around
-#the early 20s
+#the early 20s.
 
 titanic %>% filter(!is.na(Age)) %>%
   ggplot(aes(x = Age, y = ..count.., fill = Survived)) +
@@ -47,8 +51,9 @@ titanic %>% filter(!is.na(Age)) %>%
   ylab("Count of People")+
   ggtitle("Survival Status by Age (Age = NA excluded)")+
   scale_fill_discrete(name = "Survival Status", labels = c("Did Not Survive", "Survived"))
-#Here we see that the only age group more likely to survive than die is the younger age group betwen 0-9 or so.  The older age groups are more likely to die
-#than survive, but not by much.  Unsruprisingly, the age group most likely to die than survive is the middle age group of about 15-35 years old.
+#Here we see that the only age groups more likely to survive than die are the younger age group between 0-9 or so
+#and the very oldest segment of the population.  The older age groups up until the oldest individuals are more likely to die
+#than survive, but not by much.  Unsurprisingly, the age group most likely to die than survive is the middle age group of about 15-35 years old.
 
 titanic %>%
   ggplot(aes(x = Pclass, fill = Survived)) +
@@ -59,7 +64,7 @@ titanic %>%
   ggtitle("Survival Count by Passenger Class")
 
 #We also see that probability of survival is related to passenger class, with 1st class being most likely to survive, and 3rd class least likely to survive.
-#We can illustrate this a little more lcearly with a pair of position fill bar plots.
+#We can illustrate this a little more clearly with a pair of position fill bar plots.
 
 
 
@@ -84,7 +89,7 @@ titanic %>%
 
 titanic %>%
   filter(Fare > 0) %>%
-  ggplot(aes(Survived_YN, Fare)) +
+  ggplot(aes(Survived_YN, Fare, group = Survived_YN)) +
   geom_boxplot(alpha = 0.2) +
   scale_y_continuous(trans = "log2") +
   geom_point() + 
@@ -104,8 +109,8 @@ titanic %>% filter(!(is.na(Age))) %>%
   xlab("Age")+
   ylab("Proportion of Age Group")+
   facet_grid(Pclass ~ Sex)
-#Note that we show firt class in the top row, second class in the middle row, and third class in the bottom row.  We show females on the left and
+#Note that we show first class in the top row, second class in the middle row, and third class in the bottom row.  We show females on the left and
 #males on the right.  These visualizations show that first and second class females are significantly more likely to survive than die.  Third class females are
-#about equally likely to survive ro die, with the older age group being more likely to die.  A similar pattern is seen in first class males.  Second and third
+#about equally likely to survive ro die, with the older age group being more likely to die.  Second and third
 #class males are much more likely to die than survive, with the exception of very young second class males.
 
