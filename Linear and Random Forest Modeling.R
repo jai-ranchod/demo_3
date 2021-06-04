2cb57299d55120d21aef37fa4567e2ac25f852da
jai-ranchod <75948545+jai-ranchod@users.noreply.github.com>
2021-06-04 09:54:16 -0400

    Updating comments

--- Linear and Random Forest Modeling.R
+++ Linear and Random Forest Modeling.R
@@ -1,5 +1,6 @@
 #####Loading Required Libraries#####
-
+#First we're simply going to load in the necessary libraries for our linear modeling and random forest
+#analysis
 if (!require("pedometrics")) install.packages("pedometrics")
 if (!require("dplyr")) install.packages("dplyr")
 if (!require("caret")) install.packages("caret")
@@ -15,6 +16,7 @@ library(corrplot)
 
 
 #####Data Set Introduction and Objective#####
+#First we'll load in the dataset, then we'll introduce it.
 d<-"https://docs.google.com/spreadsheets/d/0BxQfpNgXuWoIWUdZV1ZTc2ZscnM/export?format=csv"
 Lung_Capacity<-read.csv(d)
 
@@ -27,6 +29,8 @@ colnames(Lung_Capacity)[5] <- "Male_YN"
 colnames(Lung_Capacity)[6] <- "Caesarean_YN"
 View(Lung_Capacity)
 #This data set evaluates lung capacity for individuals with differing medical and demographic characteristics
+#It is pulled from the "Marin Stats" website, found here:
+#https://www.statslectures.com/
 
 #LungCap - Lung capacity
 #Age - Age of subject
@@ -35,13 +39,18 @@ View(Lung_Capacity)
 #Male_YN - 1 if the subject is male, 0 otherwise
 #Caesarean_YN - 1 if the subject was a caesarean birth,  otherwise
 
-max(Lung_Capacity$Age)
 min(Lung_Capacity$Age)
+max(Lung_Capacity$Age)
+
 #Notice that the age range for this data set is 3-19
 #The objective is to build a machine learning model to predict lung capacity based on available predictors.
 #We start with a linear model, then move on to a random forest model.
 
 #####Linear Modeling:  Checking Assumptions of Linear Modeling#####
+#The first thing we need to do is check for collinearity.  We will do this by first 
+#converting the binary predictors to factors, then checking the
+#variance inflation factors.
+
 Capacity <- as.data.frame(Lung_Capacity)
 Capacity$Smoker_YN <- as.factor(Capacity$Smoker_YN)
 Capacity$Male_YN <- as.factor(Capacity$Male_YN)
@@ -52,18 +61,23 @@ stepVIF(Model)
 
 Model <- stepVIF(Model)
 
-#All of our predictors meet our threshold and can stay in the model for now.  Next we see what the model looks like.
+#For these purposes, we'll keep the stepVIF default threshold of 10.
+#All of our predictors meet our threshold and can stay in the model for now. 
+#Next we see what the model looks like.
 
 summary(Model)
 
-#Here we see that we are describing approximately 85% of the variation in lung capacity with our predictors.  We further see that we have a highly statistically significant F-statistic.
-#Finally, we note that all of our predictors have a p-value less than 0.05, indicating they are all statistically significant.  If this were not the case, we would begin the "backward selection" process by removing the predictor with the highest p-value and re-running the model until all predictors were below the significance threshold.
-#Unsurprisingly we note that being a smoker reduces lung capacity.  We note as well that age increases lung capacity which may seem counterintuitive, until we see that the maximum age in this dataset is 19.
+#Here we see that we are describing approximately 85% of the variation in lung capacity with our predictors. 
+#We further see that we have a highly statistically significant F-statistic.
+#Unsurprisingly we note that being a smoker reduces lung capacity. 
+#We note as well that age increases lung capacity which may seem counter-intuitive, until we recall that the maximum age in this dataset is 19.
+#Recall running the following code:
 
 min(Lung_Capacity$Age)
 max(Lung_Capacity$Age)
 
-#We finally must make sure we are meeting the assumptions of linear modeling.  We do not know anything about the data collection process, so we cannot be sure that the samples are genuinely independent.
+#We finally must make sure we are meeting the assumptions of linear modeling.  
+#We do not know anything about the data collection process, so we cannot be sure that the samples are genuinely independent.
 #However, we will assume for now that they are.  Next, we can generate diagnostic plots with the simple plot() function.
 
 par(mfrow = c(2,2))
@@ -76,6 +90,9 @@ par(mfrow = c(1,1))
 
 #####Linear Modeling: Splitting into test set and train set#####
 #Now we split the data into a training set and a testing set.
+#Before performing cross-validation to determine optimal model parameters, we need to split
+#the data into a training set and a test set.  Cross validation will be performed entirely within
+#the training set, then the final model will be tested against the test set.
 set.seed(1)
 y <- Capacity$LungCap
 testIndex <- createDataPartition(y, times = 1, p = 0.2, list = FALSE)
@@ -89,16 +106,23 @@ train_y <- train$LungCap
 test_x <- test[,-1]
 test_y <- test$LungCap
 #####Linear Modeling: Training a linear model#####
+#Here we train the model with the train() function that cross-validates the model by partitioning
+#the training set into one smaller training set and one validation set, in this case 5 times over
+#meaning we are using "5-fold" cross-validation.
+
 set.seed(3)
 control <- trainControl(method="cv", number = 5)
 LinearModel <- train(train_x, train_y, method = "lm", trControl = control)
 
 summary(LinearModel)
-#Notice that age seems to increase predicted lung capacity.  This may seem counterintuitive until we
-#recall that the ages of the participants span from 3 to 19.
-#From the summary of our linear model we can see that we explain 86% of the variance in lung capacity with 
-#the features in our model.  This is a good start, now let's see how we do against our test set.
+
+
+#From the summary of our linear model we can see that we explain 85% of the variance in lung capacity with 
+#the features in our model, as we saw above.
+#This is a good start, now let's see how we do against our test set.
 #####Linear Modeling: Evaluating the linear model#####
+#Here we run the model produced by cross-validation against the test set that we held out at
+#the very beginning.
 
 lmPredictions <- predict(LinearModel, test_x)
 
@@ -111,16 +135,17 @@ min(Capacity$LungCap)
 #Given that we have a range of over 14, an RMSE of slightly uner 1 is a strong performance for our linear model.
 
 #####Random Forest: Training the model#####
+#Here we try to predict the same outcome on the same dataset using a random forest model.
 #We will use the same train-test split established in the context of the linear model above.
 #We will attempt to optimize both node size and number of trees.  Given that we have only 5 predictors
-#we can try 1-10 node size  and 1-5 randomly selected
-#predictors
+#we can try 1-10 node size  and 1-5 randomly selected predictors.
+#First we need to see how many trees we will need in order to stabilze prediction accuracy.
 set.seed(2)
 
 fit <- randomForest(LungCap~., data = Capacity)
 plot(fit)
 
-#The fit of the plot indicates we will need about 250 trees to stabilize prediction accuracy
+#The fit of the plot indicates we will need about 300 trees to stabilize prediction accuracy
 set.seed(4)
 control <- trainControl(method="cv", number = 5)
 grid <- data.frame(mtry = c(1,2,3,4,5))
@@ -128,13 +153,26 @@ nodeSize <- seq(1,30,1)
 rfRMSE <- sapply(nodeSize, function(ns){
   rf <- train(train_x, train_y,
               method = "rf",
-              ntree = 250,
+              ntree = 300,
               nodesize = ns,
               tuneGrid = grid,
               trControl = control)$results["RMSE"]
 })
 
-results <- as.data.frame(rbind(rfRMSE[[1]], rfRMSE[[2]], rfRMSE[[3]], rfRMSE[[4]], rfRMSE[[5]], rfRMSE[[6]], rfRMSE[[7]], rfRMSE[[8]], rfRMSE[[9]], rfRMSE[[10]], rfRMSE[[11]], rfRMSE[[12]], rfRMSE[[13]], rfRMSE[[14]], rfRMSE[[15]], rfRMSE[[16]], rfRMSE[[17]], rfRMSE[[18]], rfRMSE[[19]], rfRMSE[[20]], rfRMSE[[21]], rfRMSE[[22]], rfRMSE[[23]], rfRMSE[[24]], rfRMSE[[25]], rfRMSE[[26]], rfRMSE[[27]], rfRMSE[[28]], rfRMSE[[29]], rfRMSE[[30]]))
+df <- c(0,0,0,0,0)
+for(i in 1:30)
+{
+  df <- as.data.frame(rbind(df, rfRMSE[[i]]))
+  
+}
+df <- df[-1,]
+
+for(i in 1:30)
+{
+  rownames(df)[i] <- i
+}
+
+results <- df
 colnames(results)[1] <- "One"
 colnames(results)[2] <- "Two"
 colnames(results)[3] <- "Three"
@@ -143,27 +181,34 @@ colnames(results)[5] <- "Five"
 View(results)
 
 which(min(results) == results)
+
+#Here we see that the minimum RMSE is the 72nd entry in the table, or row 12, column 3; minimum RMSE ~ 1.077
+
 #####Random Forest: Testing and evaluating the model#####
 #In the "results" table we have the columns identified by the number of randomly selected predictors
-#(mtry) and the rows identified by node size.  The lowest RMSE we see in this table is 1.067,
-#which is found at mtry = 3, node size = 23.  Therefore, these are the parameters
+#(mtry) and the rows identified by node size.  The lowest RMSE we see in this table is 1.077,
+#which is found at mtry = 3, node size = 12.  Therefore, these are the parameters
 #we will use in the model on the test set
 testRF <- train(train_x,
                 train_y,
-                nodesize = 23,
+                nodesize = 12,
                 tuneGrid = data.frame(mtry = 3),
-                ntree = 250)
+                ntree = 300)
 
 rfPredictions <- predict(testRF, test_x)
 
 rfRMSE <- sqrt(sum((rfPredictions-test_y)^2)/length(rfPredictions))
 rfRMSE
-#We see that the random forest RMSE of 0.9897 is comparable to the linear model RMSE but slightly lower.
+
+#We see that the random forest RMSE of 1.002 is comparable to the linear model RMSE.
 
 varImp(testRF)
+
 #Notice that the feature importance almost exactly lines up with the predictor p-values in the linear model.
 #You can see this by running:
-summary(linearModel)
+
+summary(LinearModel)
+
 #again.
 #####Ensemble Prediction#####
 
@@ -180,7 +225,7 @@ ensembleRMSE
 
 (lmRMSE - ensembleRMSE)/(lmRMSE)
 
-#With an ensemble prediction, we decrease RMSE by about 0.5% over the random forest model and about 1.5% over
+#With an ensemble prediction, we decrease RMSE by about 1.6% over the random forest model and about 1.5% over
 #the linear model.
 #In the end, we can be confident that if the patterns in the data continue to hold, we can accurately predict
 #the lung capacity of individuals aged 3-19 years old.
