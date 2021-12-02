c530e5808937f4a96603e21e17ea8672447cbefd
jai-ranchod <75948545+jai-ranchod@users.noreply.github.com>
2021-12-02 10:11:26 -0500

    Adding feature engineering experimentation section.

--- KNN - Titanic.R
+++ KNN - Titanic.R
@@ -6,8 +6,8 @@
 
 
 #####Loading Libraries and Introducing Data#####
-#Here we use a logistic regression model as a means of binary classification to predict who will survive and who will not survive the infamous titanic wreck
-#of 1912.  We use the built in "titanic_train" data set to train a k-nearest neighboers model which we then test against
+#Here we are interested in solving a classification problem to predict who will survive and who will not survive the infamous titanic wreck
+#of 1912.  We use the built in "titanic_train" data set to train a k-nearest neighbors model which we then test against
 #a held out test subset of the "titanic_train" data set.
 
 if (!require("titanic")) install.packages("titanic")
@@ -18,6 +18,9 @@ if (!require("dplyr")) install.packages("dplyr")
 if (!require("ggplot2")) install.packages("ggplot2")
 if (!require("tidyr")) install.packages("tidyr")
 if (!require("pedometrics")) install.packages("pedometrics")
+if (!require("corrplot")) install.packages("corrplot")
+if (!require("caret")) install.packages("caret")
+
 
 library(titanic)
 library(splines)
@@ -26,6 +29,8 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(pedometrics)
+library(corrplot)
+library(caret)
 #First we process the titanic_train data set
 titanic <-  titanic_train %>%
   select(Survived, Pclass, Sex, Age, SibSp, Parch, Fare)
@@ -37,6 +42,23 @@ titanic <- na.omit(titanic)
 #making the sex column binary with male = 1
 titanic$sex_binary <- as.integer(titanic$Sex == "male")
 
+#We also want to make sure the we have a set of predictors that are independent of each other.  The first thing we can do is create a correlation 
+#matrix to analyze how closely aligned some of these predictors may be.
+titanic_corr <- titanic[,c(1,2,4,5,6,7,8)]
+Matrix <- cor(titanic_corr)
+corrplot(Matrix)
+
+#We notice two reasonably strong relationships here.  The first is between sex and survival.  This is not surprising, nor will it 
+#present a problem for our model.  The second is between Fare and Class.  This also makes sense given that higher class tickets cost more.
+#Let's focus on this and see exactly how much of a correlation is present.
+
+cor(titanic$Fare, titanic$Pclass)
+
+#The correlation has a magnitude of about 0.55 which is actually not as much of an issue as one may have thought initially.  It's difficult to tell
+#what to do here.  Clearly a higher class ticket will generally cost more money, indicating we should remove the Fare predictor since class is ultimately
+#what would be more indicative of survival.  However, it is possible that within classes there would be more or less desirable cabins that would have different
+#prices and also influence survival status, indicating we should keep the Fare predictor in the model.  Let's keep the Fare predictor active for now, and we can experiment
+#with it later.
 
 #####Normalizing data#####
 #It is important in KNN to normalize data so that the range of an attribute does not disproportionately
@@ -52,7 +74,7 @@ hist(titanic$Age, main = "Histogram of Age", xlab = "Age", ylab = "Count of Pass
 #This distribution is much easier to handle.  
 
 #However, given the outliers in the "Fare" attribute, we should
-#try out the "robust scaler" noramlization method.
+#try out the "robust scaler" normalization method.
 #####Robust Normalization#####
 
 #We use the following formula:
@@ -247,8 +269,162 @@ MaxMinKappa
 #unexpected since the max/min method coerces all predictors to the same scale, whereas the robust normalization method
 #does not.  We do notice some outliers in the "fare" predictor, but evidently these outliers are not enough to make
 #the robust normalization method more effective than the max/min method.
-#Now let's try both methods simultaneously.  That is, we'll transform via the robust methodology first, then from there
-#coerce everything to the same scale via the max/min method.
+
+#####Experimenting with different predictor subsets#####
+
+#Now let's return to the question of what to do about the correlation between the Fare and Class predictors.  Let's see if removing the Fare
+#predictor or merging the two yields a higher test accuracy.  First let's try simply removing the Fare predictor.
+
+titanicNorm_test1 <- titanicNorm[,c(1:6)]
+set.seed(5)
+vec <- sample(c(1:nrow(titanicNorm_test1)), (nrow(titanicNorm_test1)/5),replace = FALSE)
+testNorm1 <- titanicNorm_test1[vec,]
+trainNorm1 <- titanicNorm_test1[-vec,]
+
+
+
+set.seed(6)
+control <- trainControl(method = "cv", number = 10, p = .9, classProbs = TRUE)
+train_knn1 <- train(Survived ~ ClassNorm + SexNorm + AgeNorm + SibNorm + ParentChildNorm, method = "knn",
+                   data = trainNorm1,
+                   tuneGrid = data.frame(k = seq(1, 71, 2)),
+                   trControl = control,
+                   metric = "Kappa") 
+
+ggplot(train_knn1, highlight = TRUE)+
+  xlab("k (Neighbors)")+
+  ylab("Kappa")+
+  ggtitle("Kappa - Max/Min Normalization")
+
+train_knn1$bestTune
+#Again, we get an interesting result in regards to optimal k.  The previous max/min test had k=3, now we go up to k=19 by removing the Fare predictor.
+
+
+knn_maxmin_fit1 <- knn3(Survived ~ ., data = trainNorm1, k = train_knn1$bestTune$k)
+y_hat_knn1 <- predict(knn_maxmin_fit1, newdata = testNorm1, type = "class")
+
+MaxMinAccuracy1 <- confusionMatrix(y_hat_knn1, testNorm1$Survived)$overall[["Accuracy"]]
+MaxMinKappa1 <- confusionMatrix(y_hat_knn1, testNorm1$Survived)$overall[["Kappa"]]
+MaxMinAccuracy1
+MaxMinKappa1
+
+
+(MaxMinAccuracy-MaxMinAccuracy1)/MaxMinAccuracy
+#Here we see that simply removing the Fare predictor lowered accuracy by about 8.6% indicating removing the Fare predictor may not be the best
+#move.  It appears there is additional information in the Fare predictor that we need.
+#For the sake of being thorough, let's try removing the Class predictor as well.
+
+titanicNorm_test2 <- titanicNorm[,c(1,3,4,5,6,7)]
+set.seed(7)
+vec <- sample(c(1:nrow(titanicNorm_test2)), (nrow(titanicNorm_test2)/5),replace = FALSE)
+testNorm2 <- titanicNorm_test2[vec,]
+trainNorm2 <- titanicNorm_test2[-vec,]
+
+
+
+set.seed(8)
+control <- trainControl(method = "cv", number = 10, p = .9, classProbs = TRUE)
+train_knn2 <- train(Survived ~ FareNorm + SexNorm + AgeNorm + SibNorm + ParentChildNorm, method = "knn",
+                    data = trainNorm2,
+                    tuneGrid = data.frame(k = seq(1, 71, 2)),
+                    trControl = control,
+                    metric = "Kappa") 
+
+ggplot(train_knn2, highlight = TRUE)+
+  xlab("k (Neighbors)")+
+  ylab("Kappa")+
+  ggtitle("Kappa - Max/Min Normalization")
+
+train_knn2$bestTune
+#The model with all of the predictors shows k=3, as does this one.
+
+knn_maxmin_fit2 <- knn3(Survived ~ ., data = trainNorm2, k = train_knn2$bestTune$k)
+y_hat_knn2 <- predict(knn_maxmin_fit2, newdata = testNorm2, type = "class")
+
+MaxMinAccuracy2 <- confusionMatrix(y_hat_knn2, testNorm2$Survived)$overall[["Accuracy"]]
+MaxMinKappa2 <- confusionMatrix(y_hat_knn2, testNorm2$Survived)$overall[["Kappa"]]
+MaxMinAccuracy2
+MaxMinKappa2
+
+
+(MaxMinAccuracy-MaxMinAccuracy2)/MaxMinAccuracy
+
+#Removing the class predictor only generates a very similar accuracy and kappa to the "baseline" model with both predictors present independently.
+
+
+#Now let's try merging the two predictors by taking the average their normalized values as a new predictor that we'll call "Class_Detail".
+#Note that since Fare and Class have a negative correlation (1st class is the most expensive, 3rd class is the least expensive) We'll need to flip
+#the direction of one of the vectors before proceeding.  It's easiest to do with the Class predictor, so we'll start there.
+
+titanicNorm_test3 <- titanicNorm
+titanicNorm_test3$Class_revised <- abs(titanicNorm_test3$Class-1)
+titanicNorm_test3 <- titanicNorm_test3 %>% mutate(Class_Detail = (Class_revised + FareNorm)/2)
+titanicNorm_test3 <- titanicNorm_test3[,c(1,3,4,5,6,9)]
+
+
+set.seed(9)
+vec <- sample(c(1:nrow(titanicNorm_test3)), (nrow(titanicNorm_test3)/5),replace = FALSE)
+testNorm3 <- titanicNorm_test3[vec,]
+trainNorm3 <- titanicNorm_test3[-vec,]
+
+
+
+set.seed(10)
+control <- trainControl(method = "cv", number = 10, p = .9, classProbs = TRUE)
+train_knn3 <- train(Survived ~ Class_Detail + SexNorm + AgeNorm + SibNorm + ParentChildNorm, method = "knn",
+                    data = trainNorm3,
+                    tuneGrid = data.frame(k = seq(1, 71, 2)),
+                    trControl = control,
+                    metric = "Kappa") 
+
+ggplot(train_knn3, highlight = TRUE)+
+  xlab("k (Neighbors)")+
+  ylab("Kappa")+
+  ggtitle("Kappa - Max/Min Normalization")
+
+train_knn3$bestTune
+#Note that k=17 in this context
+
+knn_maxmin_fit3 <- knn3(Survived ~ ., data = trainNorm3, k = train_knn3$bestTune$k)
+y_hat_knn3 <- predict(knn_maxmin_fit3, newdata = testNorm3, type = "class")
+
+MaxMinAccuracy3 <- confusionMatrix(y_hat_knn3, testNorm3$Survived)$overall[["Accuracy"]]
+MaxMinKappa3 <- confusionMatrix(y_hat_knn3, testNorm3$Survived)$overall[["Kappa"]]
+MaxMinAccuracy3
+MaxMinKappa3
+
+
+(MaxMinAccuracy-MaxMinAccuracy3)/MaxMinAccuracy
+
+
+
+#We see that we have a lower test accuracy with a merged predictor than if we were to keep the predictors separate; 8.55%% lower.  
+
+Orthog <- data.frame()
+Orthog[1,1] <- "Accuracy"
+Orthog[2,1] <- "Kappa"
+Orthog[1,2] <- MaxMinAccuracy
+Orthog[2,2] <- MaxMinKappa
+Orthog[1,3] <- MaxMinAccuracy1
+Orthog[2,3] <- MaxMinKappa1
+Orthog[1,4] <- MaxMinAccuracy2
+Orthog[2,4] <- MaxMinKappa2
+Orthog[1,5] <- MaxMinAccuracy3
+Orthog[2,5] <- MaxMinKappa3
+colnames(Orthog)[1] <- "Metric"
+colnames(Orthog)[2] <- "AllPredictors"
+colnames(Orthog)[3] <- "RemovedFare"
+colnames(Orthog)[4] <- "RemovedClass"
+colnames(Orthog)[5] <- "Merged"
+View(Orthog)
+
+#The tradeoff we're trying to optimize here is lost information by removing or merging predictors versus the risk of skewing results by including
+#multiple predictors that have a correlation magnitude around 0.55.  By experimentation, we see that this tradeoff is optimized by keeping both
+#predictors in the model independently.  Apparently, feature engineering forces us to lose information, and this hurts our model more that including the 
+#predictors correlated at magnitude 0.55 would. We see that removing the class predictor does not cost us much accuracy, but it is still not the 
+#best option in terms of cross-validated test results.  Therefore, we say the final model should include the full set of predictors initially defined.
+
+
 #####Ensemble Normalization#####
 titanic <-  titanic_train %>%
   select(Survived, Pclass, Sex, Age, SibSp, Parch, Fare)
